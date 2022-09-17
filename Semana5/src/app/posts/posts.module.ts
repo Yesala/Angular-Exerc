c6759fcfefdff4467d5e63d30b2f1b2ace5f0d02
Yesala <ysalazarj@ucenfotec.ac.cr>
2022-09-16 22:19:22 -0600

    Semana 5 Update

--- Semana5/src/app/posts/posts.module.ts
+++ Semana5/src/app/posts/posts.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PostsComponent } from './pages/posts/posts.component';
-import { PostDetailComponent } from './pages/post-detail/post-detail.component';
 import { AboutComponent } from './pages/about/about.component';
 import { NotfoundComponent } from './pages/notfound/notfound.component';
 
