92cca802fcb4fb6e8d6c970420676bc5d342c65f
Yesala <ysalazarj@ucenfotec.ac.cr>
2022-10-08 18:22:52 -0600

    ComponenteReutilizable-Proyecto

--- Semana8/src/app/app.module.ts
+++ Semana8/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { AboutComponent } from './about/about.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { MoviesModule } from './movies/movies.module';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -19,6 +22,10 @@ import { MoviesModule } from './movies/movies.module';
     NotFoundComponent
   ],
   imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
